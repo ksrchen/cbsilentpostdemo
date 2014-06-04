5baab2e73880a5b905b59325de557a68aedb44ea
kevin chen <ksrchen@gmail.com>
2014-06-04 08:11:01 -0700

    nclude file

---
+++ mpps/Controllers/MppsBaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace mpps.Controllers
+{
+    public abstract class MppsBaseController : ApiController
+    {
+        public models.Profile Profile { get; set; }
+    }
+}
