45b495376344f85591e7f2e7eb171015ead58ede
kevin chen <ksrchen@gmail.com>
2014-06-11 15:06:25 -0700

    add logging

--- mpps/Filters/AuthorizationFilter.cs
+++ mpps/Filters/AuthorizationFilter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
@@ -14,6 +16,8 @@ namespace pts
 {
     public class AuthorizationFilter : FilterAttribute, IAuthorizationFilter
     {
+        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(typeof(AuthorizationFilter));
+
         IProfileDomain _profileDomain = new ProfileDomain();
         public Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext,
             CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
@@ -62,15 +66,45 @@ namespace pts
                     throw new Exception(string.Format("Profile {0} not found ", profileId));
                 }
 
+                _logger.Info("authorization granted. " + getContext(actionContext.Request.Headers));
                 return continuation();
             }
             catch (Exception exp)
             {
+                _logger.Error("authorization failed." + getContext(actionContext.Request.Headers) + exp.Message, exp);
                 actionContext.Response = actionContext.Request.CreateErrorResponse(System.Net.HttpStatusCode.Forbidden, exp);
                 var source = new TaskCompletionSource<HttpResponseMessage>();
                 source.SetResult(actionContext.Response);
                 return source.Task;
             }
+        }
+
+        private string getContext(HttpRequestHeaders headers)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                var host = headers.FirstOrDefault(p => p.Key == "Host").Value.First();
+                if (!string.IsNullOrWhiteSpace(host))
+                {
+                    sb.AppendFormat("Host: {0} ", host);
+                }
+                var authorizationHeader = headers.FirstOrDefault(p => p.Key == "Authorization").Value.First();
+                if (!string.IsNullOrWhiteSpace(authorizationHeader))
+                {
+                    var parts = authorizationHeader.Split(new char[] { ':' });
+                    if (parts.Count() > 0)
+                    {
+                        sb.AppendFormat("profile: {0} ", parts[0]);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+            }
+
+            return sb.ToString();
+          
         }
 
         private void validateSignature(string profile, string key, string secret, string signature)
